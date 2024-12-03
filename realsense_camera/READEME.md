fc3b064a9159d1f3d7283095c22a83a70d8f570a
Asa Rogers <cyberasasoftware@gmail.com>
2024-12-03 13:08:31 -0600

    add april tags

---
+++ realsense_camera/READEME.md
@@ -0,0 +1,4 @@
+## Dependencies
+1. Apriltags
+    - install by running the following:
+    `sudo apt install ros-jazzy-apriltag-ros`
--- realsense_camera/realsense/setup.py
+++ realsense_camera/realsense/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config',['config/pcl.rviz']),
-        ('share/' + package_name + '/launch', ['launch/camera.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/camera.launch.py', 'launch/rs_launch.py']),
     ],
     install_requires=['setuptools'],  # Dependencies for Python
     zip_safe=True,
