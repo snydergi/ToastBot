081ff9f3df21c3e13d42c5fe4f6c889fb4c7f90b
Sharwin24 <sharwin24@gmail.com>
2024-12-05 10:20:18 -0600

    Updating comment

--- realsense_camera/realsense/realsense/localizer.py
+++ realsense_camera/realsense/realsense/localizer.py
@@ -10,6 +10,11 @@ import numpy as np
 # Using the Realsense camera, locate the april tag that is mounted to the robot's base
 # Now that we Know RobotBase -> BaseTag and Camera -> BaseTag,
 # We need to manually publish the tf transform from RobotBase -> BaseTag
+# And then we need to then publish the tf transform from Camera -> BaseTag
+# so we can connect the TF tree.
+# This node should not publish to any topics but should publish the TF of the Camera
+# to the BaseTag and all other tags it sees so other nodes can look up
+# RobotBase -> <AnyTag> since
 
 
 class Localizer(Node):
